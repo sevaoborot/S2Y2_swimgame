6de9eaf9abe98a50b2cb769d36544661203a29d5
Усевалад Мартысюк | Usevalad Martysiuk <wsmartysuk@gmail.com>
2023-05-17 12:12:00 +0200

    Randomizing positions added;

--- Swimming_Pool_Game/Swimming_Pool_Game/Assets/Main Folder/Scripts/Settings/Level/InisializeLevel.cs
+++ Swimming_Pool_Game/Swimming_Pool_Game/Assets/Main Folder/Scripts/Settings/Level/InisializeLevel.cs
@@ -12,6 +12,7 @@ public class InisializeLevel : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        transformShuffle(); //using Fisher-yets algorithm 
         var PlayerConfigs = PlayerConfigManager.Instance.GetPlayerConfigurations().ToArray();
         Debug.Log("Regular mode enabled");
         for (int i = 0; i < PlayerConfigs.Length; i++)
@@ -19,6 +20,17 @@ public class InisializeLevel : MonoBehaviour
             var player = Instantiate(playerPrefab, playerSpawn[i].position, playerSpawn[i].rotation, gameObject.transform);
             player.GetComponent<MovementInput>().InitializePlayer(PlayerConfigs[i]);
             player.GetComponent<MovementSystem>().SetMovementType(playerTypeOfMovement[i]); //there is a mistake of out of range, but it works, so i dont care rn, maybe fix it later
+        }
+    }
+
+    void transformShuffle()
+    {
+        for (int i = playerSpawn.Length - 1; i >= 1; i--)
+        {
+            int j = Random.Range(0, i);
+            Transform tmp = playerSpawn[j];
+            playerSpawn[j] = playerSpawn[i];
+            playerSpawn[i] = tmp;
         }
     }
 }
